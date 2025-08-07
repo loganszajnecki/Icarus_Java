487414573a16e9caa5d505a10115e4a998fb29f7
Logan Szajnecki <ljszajnecki@outlook.com>
2025-08-07 16:06:01 -0500

    untrack bin

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -14,6 +14,7 @@ public class Renderer {
 	public void render(RawModel model) {
 		GL30.glBindVertexArray(model.getVaoID());
 		GL20.glEnableVertexAttribArray(0);
+		GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
 		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
 		GL20.glDisableVertexAttribArray(0);
 		GL30.glBindVertexArray(0); // unbind
